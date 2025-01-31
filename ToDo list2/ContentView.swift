3e4fc55021a9da21408ce600e06a0e9239ca37c0
Doclock <Doctoracretg@mail.ru>
2025-01-31 21:06:44 +0300

    Add new screen

--- ToDo list2/ContentView.swift
+++ ToDo list2/ContentView.swift
@@ -1,12 +1,4 @@
-//
-//  ContentView.swift
-//  ToDo list2
-//
-//  Created by Виктория Струсь on 28.01.2025.
-//
-
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
     @StateObject private var viewModel = TaskListViewModel()
@@ -15,136 +7,161 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            
             VStack {
                 // Поиск
                 HStack {
                     Image(systemName: "magnifyingglass")
                         .foregroundColor(.gray)
+                        .padding(.leading, 10)
                     TextField("Search", text: $newTaskTitle)
+                    Image(systemName: "mic.fill")
+                        .foregroundColor(.gray)
+                        .padding(.trailing, 10)
                 }
-                .padding()
-                .background(Color.gray.opacity(0.1))
+                .frame(width: 360, height: 36)
+                .background(Color(red: 39 / 255, green: 39 / 255, blue: 41 / 255))
                 .cornerRadius(8)
+                .padding(.top, 16)
+                .padding(.leading, 6)
                 
                 // Список задач
                 List {
                     ForEach(viewModel.tasks) { task in
-                        HStack(alignment: .top) {
-                            // Чекбокс
-                            Circle()
-                                .fill(task.isCompleted ? Color.yellow : Color.clear)
-                                .frame(width: 20, height: 20)
-                                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
-                            
-                            VStack(alignment: .leading, spacing: 4) {
-                                Text(task.title)
-                                    .font(.headline)
-                                    .foregroundColor(task.isCompleted ? Color.gray : Color.white)
-                                    .strikethrough(task.isCompleted)
-                                
-                                if !task.description.isEmpty {
-                                    Text(task.description)
-                                        .font(.subheadline)
-                                        .foregroundColor(.gray)
-                                        .lineLimit(2)
+                        VStack {
+                            HStack(alignment: .top) {
+                                ZStack {
+                                    Circle()
+                                        .stroke(task.isCompleted ? Color(red: 254 / 255, green: 215 / 255, blue: 2 / 255) : Color(red: 77 / 255, green: 85 / 255, blue: 94 / 255), lineWidth: 1.5)
+                                        .frame(width: 24, height: 24)
+                                    
+                                    if task.isCompleted {
+                                        Image(systemName: "checkmark")
+                                            .resizable()
+                                            .frame(width: 12, height: 9)
+                                            .foregroundColor(Color(red: 254 / 255, green: 215 / 255, blue: 2 / 255))
+                                    }
                                 }
                                 
-                                if !task.date.isEmpty {
-                                    Text(task.date)
-                                        .font(.caption)
-                                        .foregroundColor(.gray)
+                                VStack(alignment: .leading) {
+                                    Text(task.title)
+                                        .font(.headline)
+                                        .foregroundColor(task.isCompleted ? Color.gray : Color.white)
+                                        .strikethrough(task.isCompleted)
+                                    
+                                    if !task.description.isEmpty {
+                                        Text(task.description)
+                                            .font(.subheadline)
+                                            .foregroundColor(task.isCompleted ? Color.gray : Color.white)
+                                            .lineLimit(2)
+                                    }
+                                    
+                                    if !task.date.isEmpty {
+                                        Text(task.date)
+                                            .font(.caption)
+                                            .foregroundColor(.gray)
+                                    }
                                 }
+                                .frame(maxWidth: .infinity, alignment: .leading)
                             }
-                            .padding(.leading, 8)
+                            .onTapGesture {
+                                viewModel.toggleCompletion(for: task)
+                            }
+                            
+                            Spacer(minLength: 0)
+                            // Кастомный разделитель
+                            Rectangle()
+                                .fill(Color(red: 77 / 255, green: 85 / 255, blue: 94 / 255))
+                                .frame(height: 1)
+                                .padding(.horizontal, 0)
+                                .padding(.vertical, 0)
                         }
+                        .listRowBackground(Color.black)
                         .padding(.vertical, 4)
-                        .onTapGesture {
-                            viewModel.toggleCompletion(for: task)
+                    }
+                }
+                .padding(.top, 8)
+                .listStyle(PlainListStyle())
+                
+                // Footer
+                ZStack {
+                    HStack {
+                        Spacer()
+                        Text("\(viewModel.tasks.count) Задач")
+                            .foregroundColor(.white)
+                            .font(.system(size: 14))
+                        Spacer()
+                    }
+
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            showAddTaskView.toggle()
+                        }) {
+                            Image(systemName: "square.and.pencil")
+                                .resizable()
+                                .frame(width: 22, height: 22)
+                                .foregroundColor(Color(red: 254 / 255, green: 215 / 255, blue: 2 / 255))
                         }
                     }
-                    .listRowBackground(Color.black)
-                    .listRowSeparator(.hidden)
+                    .padding(.trailing, 22) // Добавляет отступ от правого края
                 }
-                .listStyle(PlainListStyle())
-                .background(Color.black.edgesIgnoringSafeArea(.all))
-                .preferredColorScheme(.dark)
-                
-                
-                
-                // Кнопка для добавления новой задачи
-                Button(action: {
-                    showAddTaskView.toggle()
-                }) {
-                    Image(systemName: "plus")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(50)
-                }
+                .frame(height: 49)
+                .background(Color(red: 39 / 255, green: 39 / 255, blue: 41 / 255))
                 .sheet(isPresented: $showAddTaskView) {
                     AddTaskView(tasks: $viewModel.tasks)
                 }
             }
-            
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
                     Text("Задачи")
                         .foregroundColor(.white)
                         .fontWeight(.bold)
+                        .font(.system(size: 34, weight: .bold))
                         .padding(.top, 3)
                         .padding(.leading, 20)
                         .padding(.bottom, 8)
-                        .font(.system(size: 34, weight: .bold))
-                    
                 }
             }
-            .background(Color(red: 4/255, green: 4/255, blue: 4/255))
+            .background(Color.black)
+                        }
+                    }
+                }
+
+struct AddTaskView: View {
+    @Binding var tasks: [Task]
+    @Environment(\.presentationMode) var presentationMode
+    @State private var newTaskTitle = ""
+    @State private var newTaskDescription = ""
+    @State private var newTaskDate = Date()
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Новая задача")) {
+                    TextField("Название задачи", text: $newTaskTitle)
+                    TextField("Описание задачи", text: $newTaskDescription)
+                    DatePicker("Дата", selection: $newTaskDate, displayedComponents: .date)
+                }
+            }
+            .navigationTitle("Добавить задачу")
+            .navigationBarItems(trailing: Button("Сохранить") {
+                let newTask = Task(
+                    title: newTaskTitle,
+                    description: newTaskDescription,
+                    date: "\(newTaskDate)",
+                    isCompleted: false
+                )
+                tasks.append(newTask)
+                presentationMode.wrappedValue.dismiss()
+            })
             
+            .background(Color.black)
         }
     }
 }
-    
 
-
-            struct AddTaskView: View {
-                @Binding var tasks: [Task]
-                @Environment(\.presentationMode) var presentationMode
-                @State private var newTaskTitle = ""
-                @State private var newTaskDescription = ""
-                @State private var newTaskDate = Date()
-                
-                var body: some View {
-                    NavigationView {
-                        Form {
-                            Section(header: Text("Новая задача")) {
-                                TextField("Название задачи", text: $newTaskTitle)
-                                TextField("Описание задачи", text: $newTaskDescription)
-                                DatePicker("Дата", selection: $newTaskDate, displayedComponents: .date)
-                            }
-                        }
-                        .navigationTitle("Добавить задачу")
-                        .navigationBarItems(trailing: Button("Сохранить") {
-                            let newTask = Task(
-                                title: newTaskTitle,
-                                description: newTaskDescription,
-                                date: "\(newTaskDate)",
-                                isCompleted: false
-                            )
-                            tasks.append(newTask)
-                            presentationMode.wrappedValue.dismiss()
-                        })
-                    }
-                }
-            }
-            
-            struct ContentView_Previews: PreviewProvider {
-                static var previews: some View {
-                    ContentView()
-                }
-            }
-        
-    
-
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
